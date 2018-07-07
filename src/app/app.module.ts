482b7823d4cef21eded2fc7a80597bf1c1d13c70
Mike Rathbun <mikerathbun@me.com>
2018-07-07 16:53:16 -0500

    added material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MaterialModule } from './/material.module';
+import { MaterialModule } from './/material.module'
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    MaterialModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, MaterialModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule } from '@angular/common'
+import { NgModule } from '@angular/core'
+import { MatButtonModule } from '@angular/material/button'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatIconModule } from '@angular/material/icon'
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [CommonModule, MatButtonModule, MatToolbarModule, MatIconModule],
+  exports: [MatButtonModule, MatToolbarModule, MatIconModule],
+  declarations: [],
 })
-export class MaterialModule { }
+export class MaterialModule {}
+
+
