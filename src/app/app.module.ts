ea78923860b030dd111da64a327e131fe972b7e7
Mike Rathbun <mikerathbun@me.com>
2018-07-08 15:20:58 -0500

    prettier not working again

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { MaterialModule } from './/material.module'
 import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
+import { AppComponent } from './app.component';
+import { ManagerModule } from './manager/manager.module';
+import { InventoryModule } from './inventory/inventory.module';
+import { PosModule } from './pos/pos.module';
+import { UserModule } from './user/user.module'
 
 
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, MaterialModule],
+  imports: [BrowserModule, AppRoutingModule, MaterialModule, ManagerModule, InventoryModule, PosModule, UserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
-import { MatButtonModule } from '@angular/material/button'
-import { MatToolbarModule } from '@angular/material/toolbar'
-import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material'
+import { MatToolbarModule } from '@angular/material'
+import { MatIconModule } from '@angular/material'
 
 @NgModule({
   imports: [CommonModule, MatButtonModule, MatToolbarModule, MatIconModule],
