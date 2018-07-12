2a4dd05dfd139ef7a1aa54ac866d8c0719a0f0bc
Mike Rathbun <mikerathbun@me.com>
2018-07-12 18:13:25 -0500

    get the theme to work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,12 @@ import { MatIconRegistry } from '../../node_modules/@angular/material'
   selector: 'app-root',
   template: `
     <mat-toolbar color="primary">
-    <mat-icon svgIcon="lemon"></mat-icon>
+      <button mat-icon-button><mat-icon>menu</mat-icon></button>
+      <mat-icon svgIcon="lemon"></mat-icon>
       <a mat-button routerLink="/home"><h1>LemonMart</h1></a>
+      <span class="flex-spacer"></span>
+      <button mat-icon-button><mat-icon>account_circle</mat-icon></button>
+      <button mat-icon-button><mat-icon>lock_open</mat-icon></button>
     </mat-toolbar>
     <router-outlet></router-outlet>
   `,
