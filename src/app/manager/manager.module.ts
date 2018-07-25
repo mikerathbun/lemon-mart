fd8f7a96c3bb70c0031d00b6c527ead8eb4ca1a0
Mike Rathbun <mikerathbun@me.com>
2018-07-25 04:35:15 -0500

    updated routing

--- src/app/manager/manager.module.ts
+++ src/app/manager/manager.module.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
+import { ManagerHomeComponent } from './manager-home/manager-home.component'
 import { ManagerRoutingModule } from './manager-routing.module'
+import { ManagerComponent } from './manager.component'
 
 @NgModule({
   imports: [CommonModule, ManagerRoutingModule],
-  declarations: [],
+  declarations: [ManagerHomeComponent, ManagerComponent],
 })
 export class ManagerModule {}
