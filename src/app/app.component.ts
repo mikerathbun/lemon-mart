2b254cfd9a53295e8b9ef8b188ae2576fb7e4bcb
Mike Rathbun <mikerathbun@me.com>
2018-08-06 00:14:09 -0500

    yet more formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,21 @@
 import { Component } from '@angular/core'
+import { MatIconRegistry } from '@angular/material'
 import { DomSanitizer } from '@angular/platform-browser'
-import { MatIconRegistry } from '../../node_modules/@angular/material'
-
 @Component({
   selector: 'app-root',
   template: `
-    <mat-toolbar color="primary">
-      <button mat-icon-button><mat-icon>menu</mat-icon></button>
-      <a mat-icon-button routerLink="/home">
-        <mat-icon svgIcon="lemon"></mat-icon>
-        <span class="mat-h2">LemonMart</span>
-      </a>
-      <span class="flex-spacer"></span>
-      <button mat-icon-button><mat-icon>account_circle</mat-icon></button>
-      <button mat-icon-button><mat-icon>lock_open</mat-icon></button>
+  <mat-toolbar color="primary" fxLayoutGap="8px">
+    <button mat-icon-button><mat-icon>menu</mat-icon></button>
+    <a mat-icon-button routerLink="/home"><mat-icon svgIcon="lemon"></mat-icon><span class="mat-h2">LemonMart</span></a>
+    <span class="flex-spacer"></span>
+    <button mat-mini-fab routerLink="/user/profile" matTooltip="Profile" aria-label="User Profile">
+      <mat-icon>account_circle</mat-icon>
+    </button>
+    <button mat-mini-fab routerLink="/user/logout" matTooltip="Logout" aria-label="Logout">
+      <mat-icon>lock_open</mat-icon>
+    </button>
   </mat-toolbar>
-    <router-outlet></router-outlet>
+  <router-outlet></router-outlet>
   `,
 })
 export class AppComponent {
